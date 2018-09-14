ba589a7359f27fb40a2c4ab39839411e9939a97c
Ivan Loreto <i.loreto@hotmail.com>
2018-09-13 21:05:06 -0700

    update to indentations

--- index.php
+++ index.php
@@ -1,137 +1,137 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>iLoreto.me - Home</title>
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>iLoreto.me - Home</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
 
-		<link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
-		<link rel="shortcut icon" href="images/favicon.ico" />
-		<!--[if IE 6]>
-		<link href="default_ie6.css" rel="stylesheet" type="text/css" />
-		<![endif]-->
-	</head>
-	<body>
-		<div id="wrapper">
-			<div id="header">
-				<div id="logo">
-					<h1><a href="http://iloreto.me">iLoreto.me</a></h1>
-				</div>
-				<!--	<div id="menu">
-				<ul>
-				<li><a href="#" accesskey="1" title="">Home</a></li>
-				<li><a href="#" accesskey="2" title="">Work</a></li>
-				<li><a href="#" accesskey="3" title="">Hobbies</a></li>
-
-				</ul>
-				</div>-->
+	<link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="shortcut icon" href="images/favicon.ico" />
+	<!--[if IE 6]>
+	<link href="default_ie6.css" rel="stylesheet" type="text/css" />
+	<![endif]-->
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div id="logo">
+				<h1><a href="http://iloreto.me">iLoreto.me</a></h1>
 			</div>
-			<div id="page">
-				<div id="content">
-					<h2>Welcome</h2>
+			<!--	<div id="menu">
+			<ul>
+			<li><a href="#" accesskey="1" title="">Home</a></li>
+			<li><a href="#" accesskey="2" title="">Work</a></li>
+			<li><a href="#" accesskey="3" title="">Hobbies</a></li>
 
+		</ul>
+	</div>-->
+</div>
+<div id="page">
+	<div id="content">
+		<h2>Welcome</h2>
+
+		<p>
+			I am an aspiring software architect. I enjoy solving problems and building computer systems to help all kinds of people. I have been working as an Application Analyst for over 8 years. I love what I do.
+		</p>
+		<p>
+			I am a husband and a father to 2 wonderful boys in sunny California.
+		</p>
+		<!--<p><a href="#" class="button-style">Read More</a></p>-->
+		<h2>Software Developer</h2>
+		<p>
+			One of my passions is to create software solutions using a variety of technologies.
+			<br>
+			Programs talking to other prgrams, reusing components, building multi-tiered systems using Object-Oriented programming is the backbone of my coding style.
+		</p>
+		<!--<p><a href="#" class="button-style">Read More</a></p>-->
+
+	</div>
+	<div id="sidebar">
+		<div id="tbox1">
+			<h2>My Skill Set</h2>
+			<ul class="style1">
+				<li class="first">
+					<h3>Windows Applications</h3>
 					<p>
-						I am an aspiring software architect. I enjoy solving problems and building computer systems to help all kinds of people. I have been working as an Application Analyst for over 8 years. I love what I do.
+						.NET Framework (C# &amp; VBA)
 					</p>
+				</li>
+				<li>
+					<h3>Database Administration</h3>
 					<p>
-						I am a husband and a father to 2 wonderful boys in sunny California.
+						SQL Server, MySQL and Legacy MS Access
 					</p>
-					<!--<p><a href="#" class="button-style">Read More</a></p>-->
-					<h2>Software Developer</h2>
+				</li>
+				<li>
+					<h3>Web Server Applications</h3>
 					<p>
-						One of my passions is to create software solutions using a variety of technologies.
-						<br>
-						Programs talking to other prgrams, reusing components, building multi-tiered systems using Object-Oriented programming is the backbone of my coding style.
+						Apache, Linux, PHP, Drupal
 					</p>
-					<!--<p><a href="#" class="button-style">Read More</a></p>-->
+				</li>
+				<li>
+					<h3>Web Authoring</h3>
+					<p>
+						HTML5, CSS 5, JavaScript
+					</p>
+				</li>
 
-				</div>
-				<div id="sidebar">
-					<div id="tbox1">
-						<h2>My Skill Set</h2>
-						<ul class="style1">
-							<li class="first">
-								<h3>Windows Applications</h3>
-								<p>
-									.NET Framework (C# &amp; VBA)
-								</p>
-							</li>
-							<li>
-								<h3>Database Administration</h3>
-								<p>
-									SQL Server, MySQL and Legacy MS Access
-								</p>
-							</li>
-							<li>
-								<h3>Web Server Applications</h3>
-								<p>
-									Apache, Linux, PHP, Drupal
-								</p>
-							</li>
-							<li>
-								<h3>Web Authoring</h3>
-								<p>
-									HTML5, CSS 5, JavaScript
-								</p>
-							</li>
-
-						</ul>
-					</div>
-
-				</div>
-			</div>
-			<div id="three-column">
-				<div id="tbox2">
-					<h2>My Work</h2>
-
-					<ul class="style2">
-						<li class="first">
-							<h3><a target="_blank" href= "https://llu.instructure.com/eportfolios/4120">LLU - Physical Therapy Data Administration System (PTDAS)</a></h3>
-							<p>
-								An enterprise-level solution to manage departmental data, organize digital media and facilitate the coordination of clinical education for Physical Therapy at Loma Linda University.
-							</p>
-							<a target="_blank" href="https://llu.instructure.com/eportfolios/4120" class="button-style">Read More</a>
-						</li>
-
-					</ul>
-				</div>
-				<div id="tbox3">
-
-					<h2>My Hobbies</h2>
-
-					<ul class="style1">
-						<li class="first">
-							Build Lego with my boys.
-						</li>
-						<li>
-							Take day trips to Disneyland.
-						</li>
-						<li>
-							Enjoy a film with my wife.
-						</li>
-						<li>	
-							Go out for a bike ride with the family.
-						</li>
-						<li>
-							Play electric guitar and jam with my brothers.
-						</li>
-
-					</ul>
-				</div>
-			</div>
-
-			<div id="footer">
-				<p>
-					Copyright &copy; <?php echo date("Y"); ?> <a href='mailto:iloreto.me@gmail.com'>iLoreto.me</a> All rights reserved.
-					
-
-					<a target="_blank" href="https://www.linkedin.com/in/iloreto">
-					<br>
-					<img src="images/btn_liprofile_blue_80x15.png" width="80" height="15" border="0" alt="View Ivan Loreto's profile on LinkedIn"> </a>
-
-				</p>
-			</div>
+			</ul>
 		</div>
-	</body>
+
+	</div>
+</div>
+<div id="three-column">
+	<div id="tbox2">
+		<h2>My Work</h2>
+
+		<ul class="style2">
+			<li class="first">
+				<h3><a target="_blank" href= "https://llu.instructure.com/eportfolios/4120">LLU - Physical Therapy Data Administration System (PTDAS)</a></h3>
+				<p>
+					An enterprise-level solution to manage departmental data, organize digital media and facilitate the coordination of clinical education for Physical Therapy at Loma Linda University.
+				</p>
+				<a target="_blank" href="https://llu.instructure.com/eportfolios/4120" class="button-style">Read More</a>
+			</li>
+
+		</ul>
+	</div>
+	<div id="tbox3">
+
+		<h2>My Hobbies</h2>
+
+		<ul class="style1">
+			<li class="first">
+				Build Lego with my boys.
+			</li>
+			<li>
+				Take day trips to Disneyland.
+			</li>
+			<li>
+				Enjoy a film with my wife.
+			</li>
+			<li>
+				Go out for a bike ride with the family.
+			</li>
+			<li>
+				Play electric guitar and jam with my brothers.
+			</li>
+
+		</ul>
+	</div>
+</div>
+
+<div id="footer">
+	<p>
+		Copyright &copy; <?php echo date("Y"); ?> <a href='mailto:iloreto.me@gmail.com'>iLoreto.me</a> All rights reserved.
+
+
+		<a target="_blank" href="https://www.linkedin.com/in/iloreto">
+			<br>
+			<img src="images/btn_liprofile_blue_80x15.png" width="80" height="15" border="0" alt="View Ivan Loreto's profile on LinkedIn"> </a>
+
+		</p>
+	</div>
+</div>
+</body>
 </html>
